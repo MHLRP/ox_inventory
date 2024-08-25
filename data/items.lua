e16b34b0b757c01d24f64e4a7fa4042a693e9dfb
IAmDanielPeter <124053362+IAmDanielPeter@users.noreply.github.com>
2024-08-25 00:35:11 -0700

    maybe fix to banage. and fix pngs

--- data/items.lua
+++ data/items.lua
@@ -5277,12 +5277,12 @@ return {
 		label = 'Bandage',
 		weight = 115,
 		stack = true,
-		client = {
-			anim = { dict = 'missheistdockssetup1clipboard@idle_a', clip = 'idle_a', flag = 49 },
-			prop = { model = `prop_rolled_sock_02`, pos = vec3(-0.14, -0.14, -0.08), rot = vec3(-50.0, -50.0, 0.0) },
-			disable = { move = true, car = true, combat = true },
-			usetime = 2500,
-		}
+		-- client = {
+		-- 	anim = { dict = 'missheistdockssetup1clipboard@idle_a', clip = 'idle_a', flag = 49 },
+		-- 	prop = { model = `prop_rolled_sock_02`, pos = vec3(-0.14, -0.14, -0.08), rot = vec3(-50.0, -50.0, 0.0) },
+		-- 	disable = { move = true, car = true, combat = true },
+		-- 	usetime = 2500,
+		-- }
 	},
 
 	--- PRISON
