81907f9f7b7c23e0c937f767db9613b0b15fbd55
IAmDanielPeter <124053362+IAmDanielPeter@users.noreply.github.com>
2025-01-07 17:04:43 -0800

    gasmask

--- data/shops.lua
+++ data/shops.lua
@@ -1660,6 +1660,7 @@ return {
 			{ name = 'diving_fill',      price = 500,   currency = 'money' },
 			{ name = 'sprayremover',     price = 600,   currency = 'money' },
 			{ name = 'wallet',           price = 500,   currency = 'money' },
+			{ name = 'gasmask',          price = 8000,   currency = 'money' },
 			{ name = 'keyring',          price = 200,   currency = 'money' },
 			{ name = 'gps',              price = 500,   currency = 'money' },
 			{ name = 'pickaxe',          price = 1500,  currency = 'money' },
