619acf559eab0bbd456255d78059bd1d463d3105
IAmDanielPeter <124053362+IAmDanielPeter@users.noreply.github.com>
2025-05-07 05:35:23 -0700

    push pickle fix

--- data/items.lua
+++ data/items.lua
@@ -19024,4 +19024,56 @@ return {
 			tint = 7
 		}
 	},
+
+	--- PICKLE CASINOS
+
+	['blackjack_table'] = {
+		label = 'Blackjack Table',
+		weight = 1,
+		stack = true,
+		close = true,
+		description = nil
+	},
+	['baccarat_table'] = {
+		label = 'Baccarat Table',
+		weight = 1,
+		stack = true,
+		close = true,
+		description = nil
+	},
+	['poker_table'] = {
+		label = 'Poker Table',
+		weight = 1,
+		stack = true,
+		close = true,
+		description = nil
+	},
+	['roulette_table'] = {
+		label = 'Roulette Table',
+		weight = 1,
+		stack = true,
+		close = true,
+		description = nil
+	},
+	['wheel_machine'] = {
+		label = 'Wheel',
+		weight = 1,
+		stack = true,
+		close = true,
+		description = nil
+	},
+	['slot_machine'] = {
+		label = 'Slot Machine',
+		weight = 1,
+		stack = true,
+		close = true,
+		description = nil
+	},
+	['horseracing_machine'] = {
+		label = 'Horseracing Machine',
+		weight = 1,
+		stack = true,
+		close = true,
+		description = nil
+	},
 }
